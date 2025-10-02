df7998a58abe0d6fdfc9aedbdc0e5fd5bb33461c
Peter Hutsul <hythulpetro@gmail.com>
2025-10-02 21:37:52 +0300

    Added Bigabid and InMobi events tracking

--- core/utils/generateBigabidHtmlWebpackPluginConfig.js
+++ core/utils/generateBigabidHtmlWebpackPluginConfig.js
@@ -11,7 +11,33 @@ const BigabidPlaceholders = `<script src="mraid.js"></script>
   final_url: unescape("#FINAL_LANDING_URL_ESC#")  
 }</script>  
 <script src="#BIGABID_PLAYABLE_CDN_URL#"></script>  
-%{IMP_BEACON}`;
+%{IMP_BEACON}
+<script>
+  function _trackURL(url) {
+    if (url) { const trackingPixel = new Image(); trackingPixel.src = url; }
+  }
+
+  function _trackBigabidEvent(event) {
+    if (window.BIGABID_BIDTIMEMACROS && window.BIGABID_BIDTIMEMACROS[event]) _trackURL(window.BIGABID_BIDTIMEMACROS[event]);
+  }
+  var completeTriggered = false;
+  function trackComplete() {
+    if (!completeTriggered) _trackBigabidEvent('complete');
+    completeTriggered = true;
+  }
+  function initEventsForBigabid(sdk) {
+    sdk.once('boot', function () { _trackBigabidEvent('mraid_viewable')});
+    sdk.once('start', function () {_trackBigabidEvent('game_viewable')});
+    sdk.once('interaction', function () {_trackBigabidEvent('engagement')});
+    sdk.once('finish', trackComplete);
+    sdk.on('interaction', function (interactions) { if (interactions > 3) trackComplete() });
+    sdk.once('install', function () {_trackBigabidEvent('click')});
+  }
+
+  var sdkReadyInterval = setInterval(function() {
+    if (window.PlayableSDK) { initEventsForBigabid(window.PlayableSDK); clearInterval(sdkReadyInterval) }
+  }, 10)
+</script>`;
 
 exports.generateBigabidHtmlWebpackPluginConfig = function generateBigabidHtmlWebpackPluginConfig(originalHtmlContentPath) {
   originalHtmlContentPath = originalHtmlContentPath || 'src/index.html';
--- core/utils/generateInMobiHtmlWebpackPluginConfig.js
+++ core/utils/generateInMobiHtmlWebpackPluginConfig.js
@@ -17,7 +17,35 @@ const InMobiPlaceholders = `<script>
     "Spent_30_Seconds": ["$P_TIMESPENT_30"],
   };
 </script>
-<script src="mraid.js"></script>`;
+<script src="mraid.js"></script>
+<script>
+  function _trackURL(url) {
+    if (url) { const trackingPixel = new Image(); trackingPixel.src = url; }
+  }
+
+  function trackInMobiEvent(event) {
+    if (window.INMOBI_DSPMACROS && window.INMOBI_DSPMACROS[event]) _trackURL(window.INMOBI_DSPMACROS[event]);
+  }
+
+  function initEventsForInMobi(sdk) {
+    sdk.once('boot', function () { trackInMobiEvent('Ad_Load_Start')});
+    sdk.once('start', function () {trackInMobiEvent('Ad_Viewable')});
+    sdk.once('interaction', function () {trackInMobiEvent('First_Engagement')});
+    sdk.once('finish',  function () {trackInMobiEvent('Gameplay_Complete')});
+    sdk.once('install', function () {trackInMobiEvent('DSP_Click')});
+    sdk.once('start', function () {
+      setTimeout(function() {trackInMobiEvent('Spent_5_Seconds')}, 5000)
+      setTimeout(function() {trackInMobiEvent('Spent_10_Seconds')}, 10000)
+      setTimeout(function() {trackInMobiEvent('Spent_15_Seconds')}, 15000)
+      setTimeout(function() {trackInMobiEvent('Spent_20_Seconds')}, 20000)
+      setTimeout(function() {trackInMobiEvent('Spent_25_Seconds')}, 25000)
+      setTimeout(function() {trackInMobiEvent('Spent_30_Seconds')}, 30000)
+    });
+  }
+  var sdkReadyInterval = setInterval(function() {
+    if (window.PlayableSDK) { initEventsForInMobi(window.PlayableSDK); clearInterval(sdkReadyInterval) }
+  }, 10)
+</script>`;
 
 exports.generateInMobiHtmlWebpackPluginConfig = function generateInMobiHtmlWebpackPluginConfig(
   originalHtmlContentPath,
